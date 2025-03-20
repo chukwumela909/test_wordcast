0dc786692d8cb7b932dbe11bcf9e726c0aebcd43
Amirize Wisdom <97132123+chukwumela909@users.noreply.github.com>
2025-03-20 09:07:26 +0100

    bb

--- index.js
+++ index.js
@@ -16,30 +16,9 @@ mongoose.connect('mongodb+srv://amirizew:0HTwexCkSckakIPL@cluster0.atoor.mongodb
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/livestreams', livestreamRoutes);
-
-// const timeStamp = Math.round(Date.now() / 1000);
-// const timeStampInMilliseconds = Date.now();
-// const appId = 486156498;
-// const serverSecret = 'b849b631636b018288a3712a2a51c4f3';
-// const signatureNonce = crypto.randomBytes(8).toString('hex');
-
-// function GenerateUASignature(appId, signatureNonce, serverSecret, timeStamp) {
-//         const hash = crypto.createHash('md5'); // Use the MD5 hashing algorithm.
-//         const str = appId + signatureNonce + serverSecret + timeStamp;
-//         hash.update(str);
-//         // hash.digest('hex') indicates that the output is in hex format 
-//         return hash.digest('hex');
-// }
-
-// const signature = GenerateUASignature(appId, signatureNonce, serverSecret, timeStamp);
-
-// console.log(signatureNonce);
-
-// console.log(timeStamp);
-
-// console.log(signature);
-
-// console.log(`Timestamp in milliseconds: ${timeStampInMilliseconds}`);
+app.get('/api/test', (req, res) => {
+        res.send('Test endpoint is working!');
+});
 
 const PORT = process.env.PORT || 3000;
 
