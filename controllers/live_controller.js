951fcf2a33c3b96b030ffb7731eefbb598354fc0
Amirize Wisdom <97132123+chukwumela909@users.noreply.github.com>
2025-04-14 21:14:50 +0100

    prevent live, when livestream already exists

--- controllers/live_controller.js
+++ controllers/live_controller.js
@@ -10,6 +10,14 @@ const createLivestream = async (req, res) => {
         const { userId } = req.body;
         const hostId = userId; // From auth middleware
 
+        const activeLivestream = await Livestream.findOne({ hostId: userId, isActive: true });
+        if (activeLivestream) return res.status(404).json({ message: 'You alreadt have an active livestream' });
+        // Create Livestream
+        const user = await User.findOne({ userId: userId })
+
+        const hostChannel = user.channelName
+        const channelImage = user.channelImage
+
         const timeStamp = Math.round(Date.now() / 1000);
         const timeStampInMilliseconds = Date.now();
         const appId = 486156498;
@@ -28,7 +36,7 @@ const createLivestream = async (req, res) => {
 
         const params = {
             Action: 'RTMPDispatchV2',
-            StreamId: 'rtc01',
+            StreamId: channelImage,
             Sequence: timeStampInMilliseconds.toString(),
             Type: 'pull',
             AppId: '486156498',
@@ -43,19 +51,12 @@ const createLivestream = async (req, res) => {
 
         const livedata = response.data
 
-
-        // Create Livestream
-        const user = await User.findOne({userId: userId})
-
-        const hostChannel = user.channelName
-        const channelImage = user.channelImage
-
         console.log(hostChannel, channelImage)
 
         const livestream = new Livestream({ liveId, hostId, hostChannel: hostChannel, channelImage: channelImage, viewCount: 0, isActive: true });
         await livestream.save();
 
-     return   res.status(200).json({ message: 'Livestream created', livedata });
+        return res.status(200).json({ message: 'Livestream created', livedata });
     } catch (error) {
         res.status(500).json({ message: 'Error creating livestream', error });
     }
@@ -63,7 +64,8 @@ const createLivestream = async (req, res) => {
 
 const fetchLivestreams = async (req, res) => {
     try {
-        const livestreams = await Livestream.find({isActive: true});
+        const livestreams = await Livestream.find({ isActive: true });
+
         res.json(livestreams);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching livestreams', error });
