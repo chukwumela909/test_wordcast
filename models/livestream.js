ecbcd6744c018643c5944c91fa27fd666bdf11a7
Amirize Wisdom <97132123+chukwumela909@users.noreply.github.com>
2025-07-01 00:30:13 +0100

    update livestream schema: set default channelImage URL

--- models/livestream.js
+++ models/livestream.js
@@ -4,7 +4,7 @@ const livestreamSchema = new Schema({
     liveId: { type: String, required: true, unique: true },
     hostId: { type: String, required: true },
     hostChannel: { type: String, required: true },
-    channelImage: { type: String, required: true },
+    channelImage: { type: String, default: 'https://res.cloudinary.com/daf6mdwkh/image/upload/v1751326132/tgn_ahitjm.png' },
     viewCount: { type: Number, default: 0 },
     isActive: { type: Boolean, default: false },// New field added
 });
